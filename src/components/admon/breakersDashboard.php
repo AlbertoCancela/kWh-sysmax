15af6eab3417de7bb06b2f9c781448458096719c
Romeo De los Angeles <romeodelosangeles@gmail.com>
2025-07-22 20:12:03 -0600

    user & admin front updated

--- src/components/admon/breakersDashboard.php
+++ src/components/admon/breakersDashboard.php
@@ -1,4 +1,3 @@
-<section class="banner-sysmax"></section>
 <section class="table-section">
     <h3 class="table-title">HISTORIAL DE CONSUMOS POR MEDIDOR <br>(último registro)</h3>
   <table id="table-breakerDisplay">
